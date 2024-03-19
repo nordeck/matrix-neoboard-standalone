7003b85113ceaf2566dd0c827de9da087465771b
Michael Weimann <michael.weimann@nordeck.net>
2024-03-19 09:59:54 +0100

    Bootstrap repo (#1)
    Signed-off-by: Michael Weimann <michael.weimann@nordeck.net>

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2024 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const { i18n, t } = useTranslation();
+
+  return (
+    <>
+      <h1>{t('home.greeting', 'Hello')}</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+        <button onClick={() => i18n.changeLanguage('de')}>DE</button>
+        <button onClick={() => i18n.changeLanguage('en')}>EN</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
