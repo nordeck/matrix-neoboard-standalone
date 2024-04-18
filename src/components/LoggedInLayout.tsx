3e87d988c1a3532992cdc8f4fcb07ae68305edb4
Michael Weimann <michael.weimann@nordeck.net>
2024-04-18 14:56:55 +0200

    Implement start of logged in layout
    Signed-off-by: Michael Weimann <michael.weimann@nordeck.net>

---
+++ src/components/LoggedInLayout.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { styled } from '@mui/material';
+import { PropsWithChildren } from 'react';
+import { NeoBoard } from './Icons/NeoBoard';
+
+const Wrapper = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.default,
+  paddingLeft: '25px',
+  paddingRight: '25px',
+}));
+
+const ContentWrapper = styled('div')(() => ({
+  borderRadius: '8px',
+}));
+
+const Header = styled('div')(() => ({
+  paddingBottom: '34px',
+  paddingTop: '34px',
+}));
+
+export function LoggedInLayout({ children }: PropsWithChildren<{}>) {
+  return (
+    <Wrapper>
+      <Header>
+        <NeoBoard />
+      </Header>
+      <ContentWrapper role="main">{children}</ContentWrapper>
+    </Wrapper>
+  );
+}
---
+++ src/components/StandaloneThemeProvider/StandaloneThemeProvider.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { MuiThemeProvider } from '@matrix-widget-toolkit/mui';
+import { ThemeProvider, useTheme } from '@mui/material';
+import { deepmerge } from '@mui/utils';
+import { PropsWithChildren, useMemo } from 'react';
+import { standaloneLightTheme } from './theme';
+
+/**
+ * This component uses the MuiThemeProvider from matrix-widget-toolkit and
+ * then uses ApplyStandaloneThemeProvider to apply and provide the standalone custom theme.
+ */
+export function StandaloneThemeProvider({ children }: PropsWithChildren<{}>) {
+  return (
+    <MuiThemeProvider>
+      <ApplyStandaloneThemeProvider>{children}</ApplyStandaloneThemeProvider>{' '}
+    </MuiThemeProvider>
+  );
+}
+
+function ApplyStandaloneThemeProvider({ children }: PropsWithChildren<{}>) {
+  const theme = useTheme();
+
+  const standaloneTheme = useMemo(() => {
+    return deepmerge(theme, standaloneLightTheme);
+  }, [theme]);
+
+  return <ThemeProvider theme={standaloneTheme}>{children}</ThemeProvider>;
+}
