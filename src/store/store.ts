14dfaaabee80bab640a9ed7b12656c09e98703b0
Marcel <MTRNord@users.noreply.github.com>
2024-08-12 13:05:35 +0200

    Fix store type (PR-147)

--- src/store/store.ts
+++ src/store/store.ts
@@ -17,7 +17,7 @@
  */
 
 import { WidgetApi } from '@matrix-widget-toolkit/api';
-import { baseApi } from '@nordeck/matrix-neoboard-react-sdk';
+import { baseApi, shapeSizesReducer } from '@nordeck/matrix-neoboard-react-sdk';
 import { autoBatchEnhancer, configureStore } from '@reduxjs/toolkit';
 import { StandaloneApi } from '../toolkit/standalone';
 import { dashboardReducer } from './dashboard/dashboardSlice';
@@ -48,6 +48,7 @@ export function createStore({
     reducer: {
       [baseApi.reducerPath]: baseApi.reducer,
       dashboardReducer,
+      shapeSizesReducer,
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
