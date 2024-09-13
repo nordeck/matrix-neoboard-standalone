251916974835c389f7174dfa137e429bd62e4a51
MTRNord <mtrnord1@gmail.com>
2024-09-13 14:37:15 +0200

    Make vite handle CSP-Nonce placeholders
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,9 @@ if (process.env.VITE_DEV_SSL === 'true') {
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  html: {
+    cspNonce: 'CSP_NONCE',
+  },
   esbuild: {
     // needed to fix neoboard yjs errors, see: https://github.com/vitejs/vite/issues/11722
     target: 'es2020',
