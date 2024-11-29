f60a925c6f47717cd900f3442c9a5e120e94ecd8
MTRNord <mtrnord1@gmail.com>
2024-11-29 12:08:07 +0100

    Ignore JSON.stringify errors in the ErrorBoundary in case we end up with a cyclic object value due to swr devtools
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -26,7 +26,7 @@ interface ErrorBoundaryProps {
  * This is an error boundary for the suspense and specific to the search results UI.
  *
  * Due to react limitations this MUST be a class component at this time.
- * See https://17.reactjs.org/docs/concurrent-mode-suspense.html#handling-errors
+ * See https://react.dev/reference/react/Component#static-getderivedstatefromerror
  */
 export default class ErrorBoundary extends Component<
   ErrorBoundaryProps,
@@ -34,6 +34,11 @@ export default class ErrorBoundary extends Component<
 > {
   state = { hasError: false, error: null };
   static getDerivedStateFromError(error: Error) {
+    // Handle JSON.stringify errors from swr devtools. We ignore them as they are due to cyclic references in the WidgetApi object.
+    if (error.message.includes('cyclic object value')) {
+      return { hasError: false, error: null };
+    }
+
     return { hasError: true, error };
   }
 
