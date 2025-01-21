8d490c9ec01188b7349b868920726829bbf47130
Milton Moura <miltonmoura@gmail.com>
2025-01-21 07:10:40 -0100

    NEO-954: Adding SnapshotLoadStateDialog (#314)
    * Adding SnapshotLoadStateDialog
    
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>
    
    * Apply review feedback
    
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>
    
    * Revert connection state refactor
    
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>
    
    * Remove snapshotInfoReducer to be updated with matrix-neoboard-react-sdk.
    
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
    
    * Restore ref
    
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
    
    ---------
    
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
    Co-authored-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>

---
+++ src/components/SnapshotLoadStateDialog/SnapshotLoadStateDialog.tsx
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2024 Nordeck IT + Consulting GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+import { useSnapshotLoadState } from '@nordeck/matrix-neoboard-react-sdk';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const SnapshotLoadStateDialog: React.FC = () => {
+  const { t } = useTranslation();
+  const { snapshotLoadDialogOpen } = useSnapshotLoadState();
+
+  if (!snapshotLoadDialogOpen) {
+    return null;
+  }
+
+  return (
+    <Dialog open={true} disableEscapeKeyDown={true}>
+      <DialogTitle>
+        {t('snapshotLoadState.dialog.title', 'Unable to display the board')}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {t(
+            'snapshotLoadState.dialog.text',
+            'You will be able to see the contents when another user is working on the board.',
+          )}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" onClick={() => window.location.reload()}>
+          {t('snapshotLoadState.dialog.goBackButton.label', 'Go back')}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
