20f4c5884dee65a27f5010a8d4e6f7fd3c43dd9f
MTRNord <213429047+Marcel-Nordeck@users.noreply.github.com>
2025-06-13 20:27:42 +0200

    Ensure vitest doesnt run the playwright tests
    Signed-off-by: MTRNord <213429047+Marcel-Nordeck@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -21,6 +21,15 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
   test: {
+    // Note a lot of these are just defaults. Just the e2e-tests are extra
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      '**/e2e-tests/**',
+    ],
     environment: 'happy-dom',
     setupFiles: ['./src/setupTests.ts'],
     server: {
