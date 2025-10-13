0d5203c3552b1f4eb849dc18d20a1e8abfb60cf9
Milton Moura <miltonmoura@gmail.com>
2025-10-13 15:36:04 +0000

    Remove unecessary SPDX copyright tag from source code files and update product name
    Signed-off-by: Milton Moura <miltonmoura@gmail.com>

--- src/lib/oidc/index.ts
+++ src/lib/oidc/index.ts
@@ -1,22 +1,21 @@
-// SPDX-FileCopyrightText: 2024 Nordeck IT + Consulting GmbH
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 /*
  * Copyright 2024 Nordeck IT + Consulting GmbH
  *
- * NeoBoard is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option)
- * any later version.
+ * NeoBoard Standalone is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
  *
- * NeoBoard is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.
+ * NeoBoard Standalone is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
  *
  * See the GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with NeoBoard. If not, see <https://www.gnu.org/licenses/>.
+ * along with NeoBoard Standalone. If not, see <https://www.gnu.org/licenses/>.
  */
 
 export { completeOidcLogin } from './completeOidcLogin';
