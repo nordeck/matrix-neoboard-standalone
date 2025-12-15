c57a277965ac8bce3d047670c652987c589410b6
Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
2025-12-15 12:45:28 +0300

    Add `REACT_APP_LOGOUT_REDIRECT_URL` environment variable with URL to redirect after logout
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>

---
+++ .changeset/thin-scissors-impress.md
@@ -0,0 +1,5 @@
+---
+'@nordeck/matrix-neoboard-standalone': patch
+---
+
+Add `REACT_APP_LOGOUT_REDIRECT_URL` environment variable with URL to redirect after logout
--- src/components/Header/LogoutDialog.tsx
+++ src/components/Header/LogoutDialog.tsx
@@ -18,6 +18,7 @@
 
 import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { getEnvironmentUrl } from '../../lib';
 import {
   matrixClientCredentialsStorageKey,
   matrixCredentialsStorageKey,
@@ -34,15 +35,18 @@ type ExportDialogProps = {
 export function LogoutDialog({ open, onClose }: ExportDialogProps) {
   const application = useApplication();
   const { t } = useTranslation();
+  const logoutRedirectUrl = getEnvironmentUrl('REACT_APP_LOGOUT_REDIRECT_URL');
 
   const handleConfirm = useCallback(() => {
     // This should be replaced by proper lifecycle functions or something similar
-    application.destroy();
+    application.destroy(logoutRedirectUrl);
     localStorage.removeItem(matrixClientCredentialsStorageKey);
     localStorage.removeItem(oidcCredentialsStorageKey);
     localStorage.removeItem(matrixCredentialsStorageKey);
-    application.start();
-  }, [application]);
+    if (!logoutRedirectUrl) {
+      application.start();
+    }
+  }, [application, logoutRedirectUrl]);
 
   return (
     <ConfirmDialog
